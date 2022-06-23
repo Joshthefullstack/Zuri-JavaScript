49153cc3c5b90917a481e64141f96facaf27d62a
Joshthefullstack <104472181+Joshthefullstack@users.noreply.github.com>
2022-06-23 03:17:06 -0700

    Basic Calculator in Javascipt

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-console.log("Ibiam Joshua");
-console.log(160)
-console.log("Nigeria")
+// var firstNumber = 12;
+// var secondNumber = 22;
+// var result;
+
+// result = firstNumber + secondNumber;
+// console.log(result);
+
+var firstNumber = window.prompt("Enter your first number: ");
+var secondNumber = window.prompt("Enter your second number: ");
+var operand = window.prompt("Enter your operand: ");
+// var result;
+
+firstNumber = parseFloat(firstNumber);
+secondNumber = parseFloat(secondNumber);
+
+if (operand == "+")
+    alert(firstNumber + secondNumber);
+else if (operand == "-")
+    alert (firstNumber - secondNumber);
+else if (operand == "*")
+    alert (firstNumber * secondNumber);
+else if (operand == "/")
+    alert (firstNumber / secondNumber);
+else
+    alert ("Invalid operator");
